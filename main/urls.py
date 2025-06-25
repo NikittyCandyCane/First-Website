87ab0e3835fbfeaae2e8c8f35df7fe450fe791ca
Nicole Zhang <nicolezhang@macbookair.home>
2025-06-25 11:15:50 -0400

    restarted to learn whats going on

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('<int:id>', views.index, name='index'),
-    path('', views.home, name='home'),
-    path('create/', views.create, name='create'),
-    path('fun/', views.fun, name='fun'),
+    path('', views.home)
 ]
