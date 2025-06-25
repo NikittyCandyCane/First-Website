55a314503fe8a4a9dee15a713eb1c093d8968641
Nicole Zhang <nicolezhang@macbookair.home>
2025-06-25 12:42:17 -0400

    made a home page with random names

--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+import random
 from django.http import HttpResponse
 
-def home(response):
-    return HttpResponse('hello world')
+#HttpReponse : Wraps it in a response package that Django can send to the server
+#Render: First uses Django logic to fill in variables and do logic, and then also wraps it in a response package
+
+def home(request):
+    nickname = random.choice(['cutie pie', 'poopoo', 'sprinkle tart', 'peepee', 'wonderous rainbow', 'confetti bomb'])
+    return render(request, 'main/home.html', context = {'nickname' : nickname})
